c9a82af2d4509ec5f459c9537c109db66a1a1565
Aleksandr Starovoitov <starovoytov.it@mail.ru>
2021-11-23 22:21:00 +0300

    Add azure sql dbs, db migrations, vault

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -2,11 +2,14 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.eShopWeb.Infrastructure.Data;
 using Microsoft.eShopWeb.Infrastructure.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Azure.Identity;
+using Microsoft.Extensions.Configuration;
 
 namespace Microsoft.eShopWeb.Web;
 
@@ -24,8 +27,11 @@ public class Program
             try
             {
                 var catalogContext = services.GetRequiredService<CatalogContext>();
+                catalogContext.Database.Migrate();
                 await CatalogContextSeed.SeedAsync(catalogContext, loggerFactory);
 
+                var identityContext = services.GetRequiredService<AppIdentityDbContext>();
+                identityContext.Database.Migrate();
                 var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
                 var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
                 await AppIdentityDbContextSeed.SeedAsync(userManager, roleManager);
@@ -42,6 +48,11 @@ public class Program
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((context, config) =>
+            {
+                var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+                config.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
+            })
             .ConfigureWebHostDefaults(webBuilder =>
             {
                 webBuilder.UseStartup<Startup>();
